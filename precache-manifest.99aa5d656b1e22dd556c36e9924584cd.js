07978732317ddf33dbab833721583ca2d9a86f75
Ventus <namonakimono@outlook.com>
2020-07-20 17:06:04 +0800

    Initial commit

---
+++ precache-manifest.99aa5d656b1e22dd556c36e9924584cd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c3c3d2e69b4c970f2a91657b833030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c6fd8b315a82fdbc98",
+    "url": "/static/css/2.740d69ab.chunk.css"
+  },
+  {
+    "revision": "df206171859c5db07b0c",
+    "url": "/static/css/main.b0981d23.chunk.css"
+  },
+  {
+    "revision": "30c6fd8b315a82fdbc98",
+    "url": "/static/js/2.85da15e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85da15e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df206171859c5db07b0c",
+    "url": "/static/js/main.3270c551.chunk.js"
+  },
+  {
+    "revision": "d219369b38413b41b254",
+    "url": "/static/js/runtime-main.f6886161.js"
+  },
+  {
+    "revision": "007227eed47d82da8f13cb41fb7001ff",
+    "url": "/static/media/Download_on_the_Mac_App_Store_Badge_CNSC_RGB_blk_092917.007227ee.svg"
+  },
+  {
+    "revision": "b36bcf724bc71f295292ea1fbf066aa1",
+    "url": "/static/media/Download_on_the_Mac_App_Store_Badge_US-UK_RGB_blk_092917.b36bcf72.svg"
+  },
+  {
+    "revision": "aebe39554ee6cfd9aca8d73f83a7e410",
+    "url": "/static/media/app.aebe3955.png"
+  },
+  {
+    "revision": "c90fc004fb09fae49b1f3d6f3aed46ea",
+    "url": "/static/media/app.c90fc004.png"
+  },
+  {
+    "revision": "836c583843e06772f3812ee9aa417207",
+    "url": "/static/media/app_drag.836c5838.png"
+  },
+  {
+    "revision": "b96c185ab2b636bc1f7fecdfb2fa77e6",
+    "url": "/static/media/app_drag.b96c185a.png"
+  },
+  {
+    "revision": "4639ca568e7aef49822252367cbd2657",
+    "url": "/static/media/app_search.4639ca56.png"
+  },
+  {
+    "revision": "bb02f9c65f248e40e0da7950d6d70151",
+    "url": "/static/media/app_search.bb02f9c6.png"
+  },
+  {
+    "revision": "7d65df0069ce7415fb70fd1e3c9365ba",
+    "url": "/static/media/extension.7d65df00.png"
+  },
+  {
+    "revision": "daa5cac0b638e1e77c8ae6fd03cc5e0a",
+    "url": "/static/media/extension.daa5cac0.png"
+  },
+  {
+    "revision": "668c28eb6c218e842b3ec7978fb365bd",
+    "url": "/static/media/extension_search.668c28eb.png"
+  },
+  {
+    "revision": "d96bdcd780b109037d375fad9d1cd87d",
+    "url": "/static/media/extension_search.d96bdcd7.png"
+  }
+]);
